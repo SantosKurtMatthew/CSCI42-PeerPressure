07d46f804770b31c5d283ddbc703b163ba7d1ebf
SantosKurtMatthew <kurtsantos924@gmail.com>
2026-03-06 21:35:34 +0800

    Fixed buttons in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,13 @@ export default function Home() {
     <main className="flex items-center justify-center h-screen bg-neutral-950 text-white">
       <div className="flex gap-4">
         <button
-          onClick={() => router.push("/sign-up")}
+          onClick={() => router.push("/Register")}
           className="bg-white text-black font-medium px-6 py-2 rounded-md hover:bg-gray-200"
         >
           Sign Up
         </button>
         <button
-          onClick={() => router.push("/sign-in")}
+          onClick={() => router.push("/Login")}
           className="border border-white text-white font-medium px-6 py-2 rounded-md hover:bg-neutral-800"
         >
           Sign In
