95fc6467cba99fe9a79a158a798a5aa2ac9e9672
SantosKurtMatthew <kurtsantos924@gmail.com>
2026-03-08 18:01:31 +0800

    Added Focus Time and Rest Time

--- app/Pomodoro/countdownTimer.tsx
+++ app/Pomodoro/countdownTimer.tsx
@@ -1,9 +1,12 @@
 'use client'
 import React, { useState, useEffect } from "react";
 
+// Source: https://www.youtube.com/watch?v=GA2LdsTmW1k
 export function CountdownTimer(){
     const [minutes, setMinutes] = useState(0);
     const [seconds, setSeconds] = useState(0);
+    const [focusMinutes, setFocusMinutes] = useState(0);
+    const [restMinutes, setRestMinutes] = useState(0);
     const [isRunning, setIsRunning] = useState(false);
     
     useEffect(() => {
@@ -21,18 +24,38 @@ export function CountdownTimer(){
         return () => clearInterval(interval);
     }, [seconds, minutes, isRunning]);
     
-    const changeSeconds=(e)=>{
-        setSeconds(e.target.value)
-    }
-    const changeMinutes=(e)=>{
-        setMinutes(e.target.value)
+
+    const changeFocusMinutes=(e)=>{
+        setFocusMinutes(e.target.value)
     }
 
-    function startTimer(){
-        if (minutes != 0 || seconds != 0){
+    const changeRestMinutes=(e)=>{
+        setRestMinutes(e.target.value)
+    }
+
+    function startFocusTimer(){
+        setMinutes(focusMinutes);
+        if (focusMinutes > 0){
+           setIsRunning(true); 
+        } else {
+            setMinutes(focusMinutes);
+        }
+    }
+
+    function startRestTimer(){
+        setMinutes(restMinutes);
+        if (restMinutes > 0){
            setIsRunning(true); 
         } else {
             setMinutes(25);
+        }
+    }
+
+    function resumeTimer(){
+        if (minutes > 0 || seconds > 0){
+            setIsRunning(true);
+        } else {
+            setMinutes(focusMinutes)
         }
     }
 
@@ -46,7 +69,7 @@ export function CountdownTimer(){
 
     function resetTimer(){
         setIsRunning(false);
-        setMinutes(25);
+        setMinutes(focusMinutes);
         setSeconds(0);
     }
 
@@ -54,24 +77,35 @@ export function CountdownTimer(){
         <main>
             <div>Countdown Timer</div>
             <div>
-                <label>Minutes</label>
-                <input value={minutes} onChange={changeMinutes} />
+                {minutes}
             </div>
             <div>
-                <label>Seconds</label>
-                <input value={seconds} onChange={changeSeconds} />
+                {seconds}
+            </div>
+
+            <div>
+                <label>Focus Minutes</label>
+                <input value={focusMinutes} onChange={changeFocusMinutes} />
+                <label>Rest Minutes</label>
+                <input value={restMinutes} onChange={changeRestMinutes} />
             </div>
 
             {!isRunning && (
-                <button onClick={startTimer}> 
-                    Start Timer 
+                <button onClick={startFocusTimer}> 
+                    Start Focus Timer 
                 </button>
             )}
-            {isRunning && (
-                <button onClick={pauseTimer}> 
-                    Pause Timer 
+            {!isRunning && (
+                <button onClick={startRestTimer}> 
+                    Start Rest Timer 
                 </button>
             )}
+            <button onClick={resumeTimer}> 
+                    Resume Timer 
+                </button>
+            <button onClick={pauseTimer}> 
+                Pause Timer 
+            </button>
             <button onClick={stopTimer}> 
                 Stop Timer 
             </button>
